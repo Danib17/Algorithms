4b4c7bd3f7fbab0c28e3ab5f6cb0ca8a9795b107
zepold <dlopez0017@icloud.com>
2016-06-07 02:52:53 -0400

    Began scripting the first Algorithm. Got the buttons to work how they need to for the time being

--- BasicAlgorithm/ArrayReturnLargest/ArrayReturnLargest.js
+++ BasicAlgorithm/ArrayReturnLargest/ArrayReturnLargest.js
@@ -1,5 +1,37 @@
-$(document).ready(function() {
-  $.fn.TestinJQuery = function() {
-    console.log("Yeeee");
-  }
+$(function() {
+  //Add BTN
+  $("#ArrayInputsButton1_").click(function() {
+    $("#ArrayInputsButton1_").toggle();
+    $("#ArrayInputsButton2_").toggle();
+    $("#ArrayInputsButton3_").toggle();
+    $("#ArrayInputsButton4_").toggle();
+
+    $("#ArrayInputBar_").fadeIn(400);
+    $("#ArrayDisplay_").animate({
+      height: "79%",
+    }, 200);
+  });
+
+  //Enter BTN
+  $("#ArrayInputsButton2_").click(function() {
+
+  });
+
+  //Remove BTN
+  $("#ArrayInputsButton3_").click(function() {
+
+  });
+
+  //Cancel BTN
+  $("#ArrayInputsButton4_").click(function() {
+    $("#ArrayInputsButton1_").toggle();
+    $("#ArrayInputsButton2_").toggle();
+    $("#ArrayInputsButton3_").toggle();
+    $("#ArrayInputsButton4_").toggle();
+
+    $("#ArrayInputBar_").fadeOut(200);
+    $("#ArrayDisplay_").animate({
+      height: "85%",
+    }, 300);
+  })
 });
